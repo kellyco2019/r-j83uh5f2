f0cc8d8b9b8eaea150c8f23e88bbfddad5dc9bd7
kellycora <kellyc@evollve.co>
2020-10-23 17:41:58 -0500

    contador

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props); 
+
+    this.state = {counter: 0};
+
+  
+    this.updateValue = this.updateValue.bind(this);
+    
+  }
+  
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" 
+        ></textarea>
+        <div  className="counter" onKeyPress={this.updateValue} >{this.state.counter}  </div>
       </div>
-    );
+    )
   }
+ 
+  updateValue = () => {
+    this.setState({ 
+      counter: this.state.counter + 1
+    });
+  } 
 }
 
 export default App;
